4652879f290782173fb35e951a9f3ca39fa64d25
marvinvillareal <babyboymarvin@gmail.com>
2014-05-07 22:37:05 +0800

    First Commit

---
+++ src/MyClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * MyClass.cpp
+ *
+ *  Created on: May 7, 2014
+ *      Author: Sinchioco Family
+ */
+
+#include "MyClass.h"
+
+MyClass::MyClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MyClass::~MyClass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/MyClass.h
@@ -0,0 +1,19 @@
+/*
+ * MyClass.h
+ *
+ *  Created on: May 7, 2014
+ *      Author: Sinchioco Family
+ */
+
+#ifndef MYCLASS_H_
+#define MYCLASS_H_
+
+class MyClass {
+public:
+	MyClass();
+	virtual ~MyClass();
+
+
+};
+
+#endif /* MYCLASS_H_ */
